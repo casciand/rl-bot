2e723668b9fd4595f9a28127c9bfc51c320d296c
casciand <andrew.c.cascio@vanderbilt.edu>
2021-06-24 00:20:30 -0400

    added ranks command

--- cogs/rocketleague.py
+++ cogs/rocketleague.py
@@ -16,30 +16,49 @@ class RocketLeague(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def rl(self, ctx):
-        await ctx.send('rocket league')
-
-    @commands.command()
     async def ranks(self, ctx, platform, username):
-        pass
+        try:
+            player_ranks, current_season, avatar_url = get_ranks(platform, username)
+            keys = list(player_ranks.keys())
+
+            embed = discord.Embed(title=f'{username}\'s ranks:', description=f'Season {current_season - 14}')
+            embed.set_thumbnail(url=avatar_url)
+
+            for i in range(len(player_ranks)):
+                gamemode = keys[i]
+                embed.add_field(name=keys[i], value=f'{player_ranks[gamemode][0]}, {player_ranks[gamemode][1]} ({player_ranks[gamemode][2]})')
+
+            await ctx.send(embed=embed)
+        except KeyError:
+            await ctx.send('Invalid platform/name. Please try again.')
 
 
 def fetch_json(platform, username):
-    trn_link = f'https://public-api.tracker.gg/api/v2/rocket-league/standard/profile/{platform}/{username}'
+    trn_link = f'https://api.tracker.gg/api/v2/rocket-league/standard/profile/{platform}/{username}'
     api_key = os.environ['TRN_API_KEY']
     headers = {
-        'TRN-Api-Key': api_key
+        'TRN-Api-Key': api_key,
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36"
     }
 
     response = requests.get(trn_link, headers=headers)
-    print(response.content)
     json_response = json.loads(response.text)
 
     return json_response
 
 
-print(fetch_json('steam', '76561198971399813'))
-
-
 def get_ranks(platform, username):
     json_response = fetch_json(platform, username)
+    current_season = json_response['data']['metadata']['currentSeason']
+    avatar_url = json_response['data']['platformInfo']['avatarUrl']
+    player_ranks = {}
+
+    for i in range(2, 9):
+        gamemode = json_response['data']['segments'][i]['metadata']['name']
+        rank = json_response['data']['segments'][i]['stats']['tier']['metadata']['name']
+        division = json_response['data']['segments'][i]['stats']['division']['metadata']['name']
+        mmr = json_response['data']['segments'][i]['stats']['rating']['value']
+
+        player_ranks[gamemode] = [rank, division, mmr]
+
+    return player_ranks, current_season, avatar_url
