7deb9b2e225ac81f10c61f441e743af974c9b6a9
casciand <andrew.c.cascio@vanderbilt.edu>
2021-06-24 17:44:07 -0400

    added fetchers, minor improvements

--- cogs/rocketleague.py
+++ cogs/rocketleague.py
@@ -1,7 +1,4 @@
-import json
-import os
-import requests
-
+from fetchers import TRNFetcher, SteamFetcher
 from dotenv import load_dotenv
 
 import discord
@@ -16,49 +13,33 @@ class RocketLeague(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def ranks(self, ctx, platform, username):
+    async def ranks(self, ctx, platform, *, username):
+        await ctx.send('Fetching ranks...')
         try:
-            player_ranks, current_season, avatar_url = get_ranks(platform, username)
+            trn_fetcher = TRNFetcher(platform, username)  # gets json containing player info
+
+            if platform == 'steam':
+                steam_fetcher = SteamFetcher(username)
+                username = steam_fetcher.get_username()
+
+            player_ranks = trn_fetcher.get_ranks()
+            current_season = trn_fetcher.get_current_season()
+            avatar_url = trn_fetcher.get_pfp()
+
             keys = list(player_ranks.keys())
 
             embed = discord.Embed(title=f'{username}\'s ranks:', description=f'Season {current_season - 14}')
             embed.set_thumbnail(url=avatar_url)
 
-            for i in range(len(player_ranks)):
+            for i in range(len(keys)):
                 gamemode = keys[i]
-                embed.add_field(name=keys[i], value=f'{player_ranks[gamemode][0]}, {player_ranks[gamemode][1]} ({player_ranks[gamemode][2]})')
+
+                if gamemode != 'Un-Ranked':
+                    embed.add_field(name=keys[i], value=f'{player_ranks[gamemode][0]}, {player_ranks[gamemode][1]} ({player_ranks[gamemode][2]})', inline=False)
 
             await ctx.send(embed=embed)
-        except KeyError:
+            print(f'Successfully retrieved {username}\'s ranks')
+        except:
             await ctx.send('Invalid platform/name. Please try again.')
+            print(f'Failed to retrieve {username}\'s ranks')
 
-
-def fetch_json(platform, username):
-    trn_link = f'https://api.tracker.gg/api/v2/rocket-league/standard/profile/{platform}/{username}'
-    api_key = os.environ['TRN_API_KEY']
-    headers = {
-        'TRN-Api-Key': api_key,
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36"
-    }
-
-    response = requests.get(trn_link, headers=headers)
-    json_response = json.loads(response.text)
-
-    return json_response
-
-
-def get_ranks(platform, username):
-    json_response = fetch_json(platform, username)
-    current_season = json_response['data']['metadata']['currentSeason']
-    avatar_url = json_response['data']['platformInfo']['avatarUrl']
-    player_ranks = {}
-
-    for i in range(2, 9):
-        gamemode = json_response['data']['segments'][i]['metadata']['name']
-        rank = json_response['data']['segments'][i]['stats']['tier']['metadata']['name']
-        division = json_response['data']['segments'][i]['stats']['division']['metadata']['name']
-        mmr = json_response['data']['segments'][i]['stats']['rating']['value']
-
-        player_ranks[gamemode] = [rank, division, mmr]
-
-    return player_ranks, current_season, avatar_url
---
+++ fetchers.py
@@ -0,0 +1,66 @@
+import os
+import json
+import requests
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+class TRNFetcher:
+    def __init__(self, platform, username):
+        self.platform = platform
+        self.username = username
+
+        link = f'https://api.tracker.gg/api/v2/rocket-league/standard/profile/{self.platform}/{self.username}'
+        api_key = os.environ['TRN_API_KEY']
+        headers = {
+            'TRN-Api-Key': api_key,
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'
+        }
+
+        try:
+            response = requests.get(link, headers=headers)
+            json_response = json.loads(response.text)
+            self.json_response = json_response
+        except:
+            print('TRNFetcher not initialized with attribute: json_response')
+
+    def get_current_season(self):
+        return self.json_response['data']['metadata']['currentSeason']
+
+    def get_pfp(self):
+        return self.json_response['data']['platformInfo']['avatarUrl']
+
+    def get_ranks(self):
+        player_ranks = {}
+
+        for i in range(1, 9):
+            gamemode = self.json_response['data']['segments'][i]['metadata']['name']
+            rank = self.json_response['data']['segments'][i]['stats']['tier']['metadata']['name']
+            division = self.json_response['data']['segments'][i]['stats']['division']['metadata']['name']
+            mmr = self.json_response['data']['segments'][i]['stats']['rating']['value']
+
+            player_ranks[gamemode] = [rank, division, mmr]
+
+        return player_ranks
+
+
+class SteamFetcher:
+    def __init__(self, steamid):
+        self.steamid = steamid
+
+        api_key = os.environ['STEAM_API_KEY']
+        link = f'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={api_key}&steamids={self.steamid}'
+
+        try:
+            response = requests.get(link)
+            json_response = json.loads(response.text)
+            self.json_response = json_response
+            print(json_response)
+        except:
+            print('SteamFetcher not initialized with attribute: json_response')
+
+    def get_username(self):
+        return self.json_response['response']['players'][0]['personaname']
