ac5fd07376c41fbba3c38fa2c7dfe3e9fc21268a
casciand <andrew.c.cascio@vanderbilt.edu>
2021-06-23 22:56:20 -0400

    first attempt grabbing data

--- cogs/rocketleague.py
+++ cogs/rocketleague.py
@@ -1,5 +1,14 @@
+import json
+import os
+import requests
+
+from dotenv import load_dotenv
+
 import discord
 from discord.ext import commands
+
+
+load_dotenv()
 
 
 class RocketLeague(commands.Cog):
@@ -9,3 +18,28 @@ class RocketLeague(commands.Cog):
     @commands.command()
     async def rl(self, ctx):
         await ctx.send('rocket league')
+
+    @commands.command()
+    async def ranks(self, ctx, platform, username):
+        pass
+
+
+def fetch_json(platform, username):
+    trn_link = f'https://public-api.tracker.gg/api/v2/rocket-league/standard/profile/{platform}/{username}'
+    api_key = os.environ['TRN_API_KEY']
+    headers = {
+        'TRN-Api-Key': api_key
+    }
+
+    response = requests.get(trn_link, headers=headers)
+    print(response.content)
+    json_response = json.loads(response.text)
+
+    return json_response
+
+
+print(fetch_json('steam', '76561198971399813'))
+
+
+def get_ranks(platform, username):
+    json_response = fetch_json(platform, username)
